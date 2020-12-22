cf53353b90e85e6ba7152de3af320c63bed276cc
Андрей Чемисов <andreychemisov68@mail.ru>
2020-12-22 23:18:06 +0300

    Написан класс LoginPage и тест loginTest.

--- src/test/java/org/myexample/LoginPage.java
+++ src/test/java/org/myexample/LoginPage.java
@@ -1,8 +1,46 @@
 package org.myexample;
 
 
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.$;
+import org.openqa.selenium.By;
 
+
+
+// Static Page Objects pattern (Page Modules)
 public class LoginPage {
 
+     public static SelenideElement
+             //Поле ввода логина
+             loginField = $(By.name("username")),
+             // Галочка "Запомнить"
+             checkBox   = $(By.xpath("//span[text()='запомнить']")),
+             // Кнопка "Ввести пароль"
+             buttonLg = $(By.xpath("//span[text()='Ввести пароль']")),
+             // Поле ввода пароля
+             pswField = $(By.name("password")),
+             // Кнопка вход
+             buttonPsw = $(By.xpath("//span[text()='Войти']")),
+             // Кнопка Х на "Загрузка фотографии и создание подписи"
+             btnX = $(By.xpath("//div[contains(@class,'icon icon_popup-close')]"));
+
+     public static void inputLogin(String login){
+         loginField.click();
+         loginField.setValue(login);
+         checkBox.click();
+         buttonLg.click();
+
+     }
+
+     public static void inputPsw(String psw) {
+         pswField.click();
+         pswField.setValue(psw);
+         buttonPsw.click();
+
+     }
+
+     public  static  void downWinFoto() {
+         btnX.click();
+     }
 
 }
--- src/test/java/org/myexample/TestMail.java
+++ src/test/java/org/myexample/TestMail.java
@@ -1,7 +1,10 @@
 package org.myexample;
 
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -9,8 +12,19 @@ public class TestMail {
 
  @BeforeAll
  static void setup(){
+     Configuration.timeout = 50000;
+     open(ConfProperties.getProperty("loginpage"));
 
-  open(ConfProperties.getProperty("loginpage"));
+ }
+
+ @Test
+ public void loginTest() {
+    LoginPage.loginField.shouldBe(Condition.visible);
+    LoginPage.inputLogin(ConfProperties.getProperty("login"));
+    LoginPage.pswField.shouldBe(Condition.visible);
+    LoginPage.inputPsw(ConfProperties.getProperty("psw"));
+    LoginPage.btnX.shouldBe(Condition.visible);
+    LoginPage.downWinFoto();
  }
 
 }
